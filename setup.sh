5163ac552e20bd0e99fd2d8e3a8f86cdf8298a35
Don Willingham <don.willingham@gmail.com>
2016-12-26 21:42:45 -0500

    Also enable Socket

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@
 sudo apt-get update
 sudo apt-get install bluez pulseaudio-module-bluetooth python-gobject python-gobject-2
 
-# Enable Source,Sink,Media; but only once
+# Enable Source,Sink,Media,Socket; but only once
 grep -i "^Enable" /etc/bluetooth/main.conf
 if [ "$?" = "0" ] ; then
-   sudo sed -i 's/Enable=.*/Enable=Source,Sink,Media/' /etc/bluetooth/main.conf
+   sudo sed -i 's/Enable=.*/Enable=Source,Sink,Media,Socket/' /etc/bluetooth/main.conf
 else
-   sudo sed -i 's/\[General\]/[General]\nEnable=Source,Sink,Media/' /etc/bluetooth/main.conf
+   sudo sed -i 's/\[General\]/[General]\nEnable=Source,Sink,Media,Socket/' /etc/bluetooth/main.conf
 fi
 
 # Set Class = 0x00041C
